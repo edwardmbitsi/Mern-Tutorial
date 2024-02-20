a3cee9923d5e4798fb3b69d3fa5470ac0626d4ce
edwardmbitsi <mbitsiedward@gmail.com>
2024-02-20 20:04:44 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require('./models/users');
+
+mongoose.connect("mongodb+srv://mbitsiedward:nMWYlFrx45L8Q2jY@cluster0.rkkdpfa.mongodb.net/mernstack?retryWrites=true&w=majority");
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
