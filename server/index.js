393d4669dd7fd4c5f092859e5b3a98c09a1205fe
edwardmbitsi <mbitsiedward@gmail.com>
2024-02-26 22:32:28 +0300

    Initial commit1

--- server/index.js
+++ server/index.js
@@ -3,17 +3,30 @@ const app = express();
 const mongoose = require("mongoose");
 const UserModel = require('./models/users');
 
-mongoose.connect("mongodb+srv://mbitsiedward:nMWYlFrx45L8Q2jY@cluster0.rkkdpfa.mongodb.net/mernstack?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://mbitsiedward:nMWYlFrx45L8Q2jY@cluster0.rkkdpfa.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0");
 
-app.get("/getUsers", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ error: "Internal server error" });
-        } else {
-            res.json(result);
-        }
-    });
+// Use async/await syntax to handle asynchronous operations
+app.get("/getUsers", async (req, res) => {
+    try {
+        const result = await UserModel.find({});
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// Use async/await syntax to handle asynchronous operations
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    try {
+        const newUser = new UserModel(user);
+        await newUser.save();
+        res.json(newUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 const PORT = process.env.PORT || 3001;
